74ed776e006d71dfd8a65dd1b46b7807c6ef4b51
roger <rogerdelagarza121212@gmail.com>
2025-05-06 12:01:44 -0500

    fixing missing folders

---
+++ module-1/1-hello-world.py
@@ -0,0 +1,13 @@
+"""Exercise 1: Hello, World!
+
+Description:
+Write a program that prints "Hello, World!" to the console.
+This is the traditional first program for learning any programming language.
+
+Expected Output:
+Hello, World!
+
+Your code below:"""
+
+
+print(input("What is your name?"))
