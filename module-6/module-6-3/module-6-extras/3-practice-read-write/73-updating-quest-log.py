75ccc172eb2e0819a1a7688f9c3dbae8bf487336
roger <rogerdelagarza121212@gmail.com>
2025-05-10 23:02:41 -0500

    Module 6 Completed

---
+++ module-6/module-6-3/module-6-extras/3-practice-read-write/73-updating-quest-log.py
@@ -0,0 +1,35 @@
+"""Updating the Quest Log
+---------------
+Description:
+    You want to read the current quests and then append a new one to the log without deleting anything.
+
+Expected Console Output:
+    Current Quests:
+    Quest Log:
+    -----------
+    - Retrieve the lost amulet from the Sunken Temple
+    - Rescue the villager trapped in the Crystal Caverns
+    - Collect 10 wolf pelts for the town guard
+    - Escort the merchant caravan to the next village
+    New quest added: - Defeat the Goblin King hiding in the Black Hollow
+
+Expected File content After Running:
+    Quest Log:
+    -----------
+    - Retrieve the lost amulet from the Sunken Temple
+    - Rescue the villager trapped in the Crystal Caverns
+    - Collect 10 wolf pelts for the town guard
+    - Escort the merchant caravan to the next village
+    - Defeat the Goblin King hiding in the Black Hollow
+
+Task:
+    1. Define a function called `update_quest_log()
+    2. Open `quest_log.txt in read mode
+    3. Print the current quests
+    4. Then open the file in append mode
+    5. Add the line `- Defeat the Goblin King hiding in the Black Hallow\n`
+    6. Print the confirmation message
+
+!TIP: Manually Inspect the file BEFORE and AFTER running your functions
+
+Your code below here"""
